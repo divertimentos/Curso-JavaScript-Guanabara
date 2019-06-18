d62dec3b3c2efb4af14338192f2aa27566a25f8d
Guilherme Teixeira <guiemi.teixeira@gmail.com>
2019-06-18 17:33:49 -0300

    Finaliza a resolução do exercício 14

---
+++ modulo_d/12_Divertimentos/divertimento_14/script.js
@@ -0,0 +1,23 @@
+function carregar() {
+    var msg = window.document.getElementById("msg")
+    var img = window.document.getElementById("imagem")
+
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora}h!`
+
+    if(hora >= 0 && hora < 12) {
+        // Bom dia
+        img.src = "morning.png"
+        document.body.style.background = "#be6f0a"
+    } else if(hora >= 12 && hora <= 18) {
+        // Boa tarde
+        img.src = "afternoon.png"
+        document.body.style.background = "#f6e7ea"
+    } else {
+        // Boa noite
+        img.src = "night.png"
+        document.body.style.background = "#023c4a"
+
+    }
+}
