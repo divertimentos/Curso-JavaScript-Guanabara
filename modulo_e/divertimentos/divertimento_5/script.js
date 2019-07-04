97799db03f3ac29228e268c9dcb74f58fc22e836
guiemi <guiemi.teixeira@gmail.com>
2019-07-04 17:29:07 -0300

    Resolve o exercício de tabuada que nem gente

--- modulo_e/divertimentos/divertimento_5/script.js
+++ modulo_e/divertimentos/divertimento_5/script.js
@@ -1,12 +1,23 @@
 function generateTable() {
-    var number = document.getElementById("number")
-    var result = document.querySelector("option#result")
-
-    let factor = Number(number.value)
-    for (let i = 1; i <= 10; i++ ) {
-        // result.innerHTML = "Resultado:"
-        result.innerHTML += `<br>${factor} x ${i} = ${factor * i}`
+    let num = document.getElementById("number")
+    let result = document.getElementById("result")
+    if (num.value.length == 0) {
+        window.alert("Você precisa digitar um número!")
+    } else {
+        var n = Number(num.value)
+        result.innerHTML = ""
+        for (i=1; i<=10; i++) {
+            let item = document.createElement("option")
+            item.text = `${n} x ${i} = ${n*i}`
+            result.appendChild(item)
+        }
     }
+
+    // let factor = Number(number.value)
+    // for (let i = 1; i <= 10; i++ ) {
+    //     // result.innerHTML = "Resultado:"
+    //     result.innerHTML += `<br>${factor} x ${i} = ${factor * i}`
+    // }
 
     // for (i = 1; i <= 10; i++) {
     //     result.innerHTML += `<br> ${number.value} x ${i} = ${number.value * i}`
