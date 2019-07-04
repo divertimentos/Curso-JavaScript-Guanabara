d8dcf65c0207d104f70d6a4f9d4cf155b4d79b9e
guiemi <guiemi.teixeira@gmail.com>
2019-07-04 17:35:35 -0300

    Remove código não utilizado

--- modulo_e/divertimentos/divertimento_5/script.js
+++ modulo_e/divertimentos/divertimento_5/script.js
@@ -12,15 +12,4 @@ function generateTable() {
             result.appendChild(item)
         }
     }
-
-    // let factor = Number(number.value)
-    // for (let i = 1; i <= 10; i++ ) {
-    //     // result.innerHTML = "Resultado:"
-    //     result.innerHTML += `<br>${factor} x ${i} = ${factor * i}`
-    // }
-
-    // for (i = 1; i <= 10; i++) {
-    //     result.innerHTML += `<br> ${number.value} x ${i} = ${number.value * i}`
-    // }
-
 }
