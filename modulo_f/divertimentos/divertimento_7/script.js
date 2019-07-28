361227a79d39307252d7c3135ffaaf21197ea402
guiemi <guiemi.teixeira@gmail.com>
2019-07-28 15:31:32 -0300

    WIP: resolvendo exercício

--- modulo_f/divertimentos/divertimento_7/script.js
+++ modulo_f/divertimentos/divertimento_7/script.js
@@ -1,4 +1,20 @@
+// ANALISADOR DE NUMEROS
+var result = document.getElementById("result")
+var lista = document.getElementById("adicionados")
+var userInput = document.getElementById("userInput")
+var listaNumeros = new Array()
+
 function gatherNumber() {
-    let teste = document.getElementById("input")
-    
+    let item = document.createElement("option")
+    item.text = userInput.value    
+    lista.appendChild(item)
+    listaNumeros.push(userInput)
+}
+
+function showAnalysis() {
+// result.innerHTML += listaNumeros[0].value
+    let item = document.createElement("p")
+    item.text = "Ao todo, temos x números cadastrados"
+    result.appendChild(item)
+    result.innerHTML = item.value
 }
